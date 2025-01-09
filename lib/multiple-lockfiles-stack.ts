147bfd9efae965d97e8dc90eca0a970d8c4073b3
Michael Käufl <michael-k@users.noreply.github.com>
2025-01-09 10:50:56 +0100

    Add restate's service deployer

--- lib/multiple-lockfiles-stack.ts
+++ lib/multiple-lockfiles-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as restate from '@restatedev/restate-cdk';
 
 export class MultipleLockfilesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MultipleLockfilesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new restate.ServiceDeployer(this, 'ServiceDeployer', {
+      architecture: lambda.Architecture.X86_64,
+    });
   }
 }
